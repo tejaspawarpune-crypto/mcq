5e178f7aed7bc7e6ce40964bb9f64e7c9cf591de
Supriya Shinde <supriya.incronix@gmail.com>
2026-02-15 18:12:20 +0530

    change env

--- Backend/server.js
+++ Backend/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT =  "0.0.0.0";
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} 🔥`);
 });
